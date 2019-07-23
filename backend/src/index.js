f5799e17c8f9c7364aa167aef4b187f60ad8611b
Rafael Leite <rafael.leite@wexinc.com>
2019-07-23 00:53:59 -0300

    Finalizando frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const path = require('path')
 const cors = require('cors')
 
 const app = express()
-const server = require('http').Server(app)
+const server = app.listen(3333)
 const io = require('socket.io')(server)
 
 mongoose.connect(process.env.DB_HOST, {
@@ -14,13 +14,11 @@ mongoose.connect(process.env.DB_HOST, {
 
 app.use((req, res, next) => {
   req.io = io
-
   next()
 })
 
-app.use(cors())
+app.use(cors());
 
 app.use('/files', express.static( path.resolve(__dirname, '..', 'uploads', 'resized') ))
 app.use(require('./route'))
 
-app.listen(3333)
--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import io from 'socket.io-client'
 import api from '../services/api'
 
 import more from '../assets/more.svg'
@@ -14,10 +15,31 @@ class Feed extends Component {
   }
 
   async componentDidMount() {
+    this.registerToSocket()
     const response = await api.get('posts')
     this.setState({ 
       feed: response.data
     })
+  }
+
+  registerToSocket = () => {
+    const socket = io('http://localhost:3333')
+
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] })
+    })
+
+    socket.on('like', likedPost => {
+      this.setState({ 
+        feed: this.state.feed.map(post => 
+            post._id === likedPost._id ? likedPost : post
+          )
+      })
+    })
+  }
+
+  handleLike = id => {
+    api.post(`/posts/${id}/like`)
   }
 
   render() {
@@ -37,7 +59,9 @@ class Feed extends Component {
 
             <footer>
               <div className="actions">
-                <img src={like} alt=""/>
+                <button type="button" onClick={() => this.handleLike(post._id)}>
+                  <img src={like} alt="Curtir"/>
+                </button>
                 <img src={comment} alt=""/>
                 <img src={send} alt=""/>
               </div>
